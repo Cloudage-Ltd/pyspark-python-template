69a4b104ff047699dec990ab4d7897e13190ae5a
rkbalu <ramk.balu@gmail.com>
2022-12-24 23:30:01 +0000

    spark - docker updates

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
-if __name__ == '__main__':
-    print('Hello world')
+from pyspark.sql import SparkSession
+
+
+# if __name__ == '__main__':
+print('Hello world')
+
+spark = SparkSession.builder.appName('Sample_Spark_App').getOrCreate()
+
+df = spark.createDataFrame([('ram', '31')], ('name', 'number'))
+
+df.show()
