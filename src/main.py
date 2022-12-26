b1fa9c2030547af2fa47a70e71d35d4390323f35
rkbalu <ramk.balu@gmail.com>
2022-12-26 22:30:01 +0000

    Initial changes

--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
 from pyspark.sql import SparkSession
 
 
-# if __name__ == '__main__':
-print('Hello world')
-
 spark = SparkSession.builder.appName('Sample_Spark_App').getOrCreate()
 
-df = spark.createDataFrame([('ram', '31')], ('name', 'number'))
+df = spark.createDataFrame([('sample', '31')], ('name', 'number'))
 
 df.show()
